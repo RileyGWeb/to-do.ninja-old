d417795632ab228ecb14f80c26e6b0a2565ed247
Coaster132 <rileygweb@gmail.com>
2022-09-21 17:45:15 -0700

    Added routes for managing items

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+    }
 }
